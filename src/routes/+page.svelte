e9391a7f842b861bbf5868a4adaedb9888295311
Lucky Charlson Manaya (1000115822) <MANALC1@student.op.ac.nz>
2023-02-24 11:30:33 +1300

    did some coding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--CSS Styling-->
+<style>
+    h1{
+        background-color: pink;
+        color: rgb(241, 40, 151);
+        text-decoration-color: rgb(241, 40, 151);
+        font-size: 30px;
+        font-style: bold;
+        font-weight: normal;
+        text-align: center;
+    }
+
+    button
+    {
+        background-color: pink;
+        border-radius: 100px;
+    }
+</style>
+
+<!--JavaScript-->
+<script>
+    let count = 0;
+    let first_name = "Lucky";
+
+    function IncrementCount()
+    {
+        count++;
+    }
+    function Reset()
+    {
+        count = 0;
+    }
+</script>
+
+<!--HTML Structure-->
+<title>Lucky's Website</title>
+<div>
+    <h1><small>Hello</small> <i>{first_name}</i>!</h1>
+</div>
+
+<button on:click = {IncrementCount}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>
+    
+</p>
+<button on:click = {Reset} style = "background-color: red">
+    Reset
+</button>
