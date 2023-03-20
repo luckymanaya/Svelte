de952865b924720c25d16b9395b9bcf08647eaff
luckymanaya <luckymanaya@gmail.com>
2023-03-20 14:58:54 +1300

    Use img tag and put it inside section tag. Bring back javascript code for text

--- src/routes/secondPage/+page.svelte
+++ src/routes/secondPage/+page.svelte
@@ -1,7 +1,11 @@
 <!--HTML coding-->
 <body>
 <div class = "picture4"></div>
-<div class="picture5"></div>
+
+<section>
+    <img src = 'images/runway.jpg'/> 
+    <p id = "textID"></p>
+</section>
 
 <br>
 <center>
@@ -9,5 +13,16 @@
         <button class = "goBackBtn">Go Back</button></a>
 </center>
 
+<script>
+    let text = "";
+    const words = ["Fashion", "Stuff", "Here"]
+
+    for(let i of words){
+        text += i + "<br>";
+    }
+
+    document.getElementById("textID").innerHTML = text;
+
+</script>
 
 </body>
