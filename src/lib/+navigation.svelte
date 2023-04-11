9f77ea3e1c3ba7ad533053ee5efc22354275fcc3
Lucky Charlson Manaya (1000115822) <MANALC1@student.op.ac.nz>
2023-04-12 11:43:08 +1200

    Change cursor to pointer when buttons are hovered. Create dropdown menu for pages

--- src/lib/+navigation.svelte
+++ src/lib/+navigation.svelte
@@ -5,6 +5,15 @@
     <ul>
       <a href="/">Home</a>
       <a href="/about">About</a>
+      <section class = "dropdown">
+        <button class = "dropdownBtn">Pages 
+          <section class = "dropdown-content">
+            <a href = "/">Page 1</a>
+            <a href = "/secondPage">Page 2</a>
+            <a href = "/thirdPage">Page 3</a>
+          </section>
+        </button>
+      </section>
       <a href="/contact">Contact Me</a>
     </ul>
   </nav>
