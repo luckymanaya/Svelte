4edda584d3de726627a5c533588d6ce737c81ada
Lucky Charlson Manaya (1000115822) <MANALC1@student.op.ac.nz>
2023-06-02 10:48:41 +1200

    Add socials in about page. Remove socials in nav bar. Rename css styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import SocialIcons from "@rodneylab/svelte-social-icons";
+</script>
+
 <section class="about-container">
   <h1>About</h1>
   <h2>Hi There!</h2>
@@ -10,3 +14,25 @@
 <h2>Got any questions? I'd love to hear from you!</h2>
 <p>Phone Number: 12345</p>
 <p>Email: email@address.com</p>
+
+<h2>Follow my socials! &#x2661</h2>
+<section class="row-container">
+  <a href="#0">
+    <SocialIcons network="instagram" />
+  </a>
+  <a href="#0">
+    <SocialIcons network="facebook" />
+  </a>
+  <a href="#0">
+    <SocialIcons network="youtube" />
+  </a>
+  <a href="https://github.com/luckymanaya">
+    <SocialIcons network="github" />
+  </a>
+</section>
+
+<style>
+  a {
+    margin: 0 1rem 1rem;
+  }
+</style>
